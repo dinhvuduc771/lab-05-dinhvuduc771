61475e37aa46b26c496201b1bd96b5cc3b0fbf55
dinhvuduc <80947775+dinhvuduc771@users.noreply.github.com>
2021-04-23 16:16:19 +0700

    sumit

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -10,16 +10,42 @@ Ex:
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
+#include<string.h>
 int Ex1(char str[]){
 	//Your codes here
+
+    int length[50], count = 0, len = strlen(str);
+
+    for (int i = 0; i < len - 1; i++)
+    {
+        for (int j = i + 1; j < len; j++)
+        {
+            if (str[j] == str[i])
+            {
+                length[count] = j - i;
+                count++;
+                break;
+            }    
+        }        
+    }
     
+    int max = length[0];
+  
+    for (int i = 0; i < count; i++)
+    {
+        if (length[i] > max)
+            max = length[i];
+    }
+    
+    
+ 
+return max ;
 }
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *testcase = argv[1];
 	
-	printf("%d",Ex1(testcase);
+	printf("%d",Ex1(testcase));
 	return 0;
 }
