9fca932e334b0b3f07aef257e7462a2fc75129ec
dinhvuduc <80947775+dinhvuduc771@users.noreply.github.com>
2021-04-23 16:07:27 +0700

    Revert "summit"
    This reverts commit 839c8d334162f8f260dda4a0aa93bc066fc35790.

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -9,39 +9,17 @@ Ex:
 
 #include <stdio.h>
 #include <stdlib.h>
-#include<string.h>
+#include <math.h>
+
 int Ex1(char str[]){
 	//Your codes here
-    int len=strlen(str), length[len], count = 0;
-    for (int i = 0; i < len - 1; i++)
-    {
-        for (int j = i + 1; j < len; j++)
-        {
-            if (str[j] == str[i])
-            {
-                length[count] = j - i;
-                count++;
-                break;
-            }    
-        }        
-    }
     
-    int max = length[0];
-  
-    for (int i = 0; i < count; i++)
-    {
-        if (length[i] > max)
-            max = length[i];
-    }
-    
-    
-    return max;
 }
 
 int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	char *testcase = argv[1];
 	
-	printf("%d",Ex1(testcase));
+	printf("%d",Ex1(testcase);
 	return 0;
 }
--- src/Exercise2.c
+++ src/Exercise2.c
@@ -15,17 +15,6 @@ Ex:
 void Ex2(int arr[], int n, int target){
 	//Your codes here
 	
-	for(int i=0;i<n-1;i++)
-	{
-		for(int j=i+1; j<n ; j++)
-		{
-               if (arr[i]+arr[j]==target)
-			   {
-				   printf("[%d %d]",i,j);
-			   }
-			
-		}
-	}
 }
 
 int main(int argc, char *argv[]) {
